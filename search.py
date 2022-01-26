1c041d1a88a974ca07dc07ddd4a8138882274e5a
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2022-01-26 22:54:38 +0500

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,12 @@
 from thefuzz import fuzz
-from thefuzz import process
 import dtb
+from google_images_search import GoogleImagesSearch
+from io import BytesIO
+from PIL import Image
+
+
+gis = GoogleImagesSearch("AIzaSyAjWZxqnIHmZ5iBngkqOZeKBvtV2uiMWlE",
+                         "77f8f97ec10f04714")
 
 def search(name):
     names = []
@@ -10,3 +16,19 @@ def search(name):
             names.append((i, ratio))
     names.sort(key=lambda x: x[1], reverse=True)
     return names[:5]
+
+def google_search(name):
+    search_params = {
+        "q": name.lower() + " формула",
+        "safe": "high",
+        "num": 1,
+        "fileType": "png|jpg"
+    }
+    gis.search(search_params)
+    my_bytes_io = BytesIO()
+    my_bytes_io.seek(0)
+    image = gis.results()[0]
+    image.copy_to(my_bytes_io)
+    my_bytes_io.seek(0)
+    img = Image.open(my_bytes_io)
+    return img
