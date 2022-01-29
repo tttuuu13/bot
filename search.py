a94f785c512f5ec2fb40379d6b8f7e31d3aa6bcc
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2022-01-30 00:09:08 +0500

    Update search.py

--- search.py
+++ search.py
@@ -3,6 +3,7 @@ import dtb
 from google_images_search import GoogleImagesSearch
 from io import BytesIO
 from PIL import Image
+import requests
 
 
 gis = GoogleImagesSearch("AIzaSyAjWZxqnIHmZ5iBngkqOZeKBvtV2uiMWlE",
@@ -32,3 +33,33 @@ def google_search(name):
     my_bytes_io.seek(0)
     img = Image.open(my_bytes_io)
     return img
+
+def gdz_search(num):
+    try:
+        images = []
+        n = num.replace(".", " ").split()
+        for i in range(1, 5):
+            r = requests.get(f"https://reshak.ru/reshebniki/algebra/10/mordkovich2/images{i}/{n[0]}-{n[1]}.png")
+            if r.content == b'Access Denied':
+                break
+            else:
+                image_bytes = BytesIO(r.content)
+                img = Image.open(image_bytes)
+                images.append(img)
+            r = requests.get(f"https://reshak.ru/reshebniki/algebra/10/mordkovich2/images{i}/{n[0]}-{n[1]}-.png")
+            if r.content == b'Access Denied':
+                continue
+            else:
+                image_bytes = BytesIO(r.content)
+                img = Image.open(image_bytes)
+                images.append(img)
+        r = requests.get(f"https://reshak.ru/reshebniki/algebra/10/mordkovich2/images/{n[0]}-{n[1]}.png")
+        if r.content == b'Access Denied':
+            pass
+        else:
+            image_bytes = BytesIO(r.content)
+            img = Image.open(image_bytes)
+            images.append(img)
+        return images
+    except:
+        return []
